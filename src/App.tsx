ba65cd23b3418acbf1a03deb361a3220fbcd0fab
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-19 17:09:11 +0300

    change App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/main/Main";
 import {Skills} from "./layout/sections/skills/Skills";
+import {Works} from "./layout/sections/works/Works";
+import {Testimony} from "./layout/sections/testimony/Testimony";
+import {Contact} from "./layout/sections/contacts/Contacts";
+import {Slogan} from "./layout/sections/slogan/Slogan";
+import {Footer} from "./layout/footer/Footer";
 
 
 function App() {
@@ -11,6 +16,11 @@ function App() {
             <Header/>
             <Main/>
             <Skills/>
+            <Works/>
+            <Testimony/>
+            <Contact/>
+            <Slogan/>
+            <Footer/>
         </div>
     );
 }
