10a08bccb86391a62e4a74aa397e490d59b36e42
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-23 14:00:40 +0300

    change header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/logo/logo";
 import {Menu} from "../../components/menu/Menu";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
 
-const items = ["Home","Skills", "Works", "Testimony", "Contact"]
+const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <Logo/>
+                    <Menu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 
 const StyledHeader = styled.header`
-    background-color: #a2f1a2;
-    display: flex;
-    justify-content: space-between;
+    background-color: #a2f1a2;    
 `
