e419d28ca87e61809bc22e5b4a341d923682c952
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-26 15:14:29 +0300

    add font

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -12,7 +12,7 @@ export const GlobalStyle = createGlobalStyle`
     
     body {
         margin: 0;
-        font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        font-family: 'Poppins', 'Josefin Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
         'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
         sans-serif;
         -webkit-font-smoothing: antialiased;
