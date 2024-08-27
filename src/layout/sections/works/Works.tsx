c67fc4a0193996480535179d2993722e174f972e
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-27 15:03:57 +0300

    change work

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,35 +1,37 @@
 import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
-import {Menu} from "../../../components/menu/Menu";
+import {TabMenu} from "./tabMenu/TabMenu";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Work} from "./work/Work";
 import socialImg from "../../../assets/images/proj1.jpg";
 import timerImg from "../../../assets/images/proj2.jpg";
+import {Container} from "../../../components/Container";
 
 const worksItems = ["All", "LANDING PAGE", "REACT", "SPA"]
 
 export const Works = () => {
     return (
         <StyledWorks>
-            <SectionTitle>My work</SectionTitle>
-            <Menu menuItems={worksItems}/>
-            <FlexWrapper justify="space-around">
-                <Work src={socialImg}
-                      title={"Social Network"}
-                      text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-                          "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
-                <Work src={timerImg}
-                      title={"Timer"}
-                      text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. " +
-                          "Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"}/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>My work</SectionTitle>
+                <TabMenu menuItems={worksItems}/>
+                <FlexWrapper justify="space-between">
+                    <Work src={socialImg}
+                          title={"Social Network"}
+                          text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
+                              "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
+                    <Work src={timerImg}
+                          title={"Timer"}
+                          text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. " +
+                              "Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"}/>
+                </FlexWrapper>
+            </Container>
         </StyledWorks>
     );
 };
 
 const StyledWorks = styled.section`
-    min-height: 100vh;
-    background-color: #cfddf8;
+    
 `
 
