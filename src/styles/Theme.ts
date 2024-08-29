3bdb6ea40669f02a6c4571a57445cb72a87509fa
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-29 16:44:55 +0300

    change Theme

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -10,5 +10,10 @@ export const theme = {
 
         placeholderColor: "#495057"
 
-}
+    },
+
+    media: {
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)",
+    }
 }
