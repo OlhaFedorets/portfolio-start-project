3898d2e5eae19f130a2a19be9a73769a047104a6
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-03 10:53:28 +0300

    change common

--- src/styles/Common.ts
+++ src/styles/Common.ts
@@ -1,14 +1,13 @@
 import {theme} from "./Theme";
 
 type FontPropsType = {
-    family?: string,
-    weight?: number,
-    color?: string,
-    lineHeight?: number,
-    Fmin?: number,
+    family?: string
+    weight?: number
+    color?: string
+    lineHeight?: number
+    Fmin?: number
     Fmax?: number
 }
-
 
 export const font = ({family,weight, color, lineHeight, Fmin, Fmax}: FontPropsType) => `
     font-family: ${family || "Poppins"};
