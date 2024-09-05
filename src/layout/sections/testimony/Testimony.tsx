e6a1ea531cb5ac0448e1ba73e3add839416ce7cb
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-05 11:27:20 +0300

    change testimony

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,18 +4,18 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
-import {IconWrapper} from "../skills/skill/Skill";
+import {S} from "./../skills/Skills_Styles";
 import {Container} from "../../../components/Container";
 
-export const Testimony = () => {
+export const Testimony: React.FC = () => {
     return (
         <StyledTestimony>
             <Container>
                 <SectionTitle>Testimony</SectionTitle>
                 <FlexWrapper direction={"column"} align={"center"}>
-                    <IconWrapper>
+                    <S.IconWrapper>
                         <Icon iconId={"quote"}/>
-                    </IconWrapper>
+                    </S.IconWrapper>
                     <Slider/>
                 </FlexWrapper>
             </Container>
@@ -27,7 +27,7 @@ const StyledTestimony = styled.section`
     min-height: 50vh;
     background-color: #c8bbda;
 
-    ${IconWrapper} {
+    ${S.IconWrapper} {
         margin: 40px 0 72px;
     }
 `
