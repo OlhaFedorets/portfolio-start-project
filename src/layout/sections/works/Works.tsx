67c44bd4387139dc7870909217855e8cc1a5c3ae
OlhaFedorets <fedoretsolha@gmail.com>
2024-09-09 16:10:46 +0300

    change works

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SectionTitle} from "../../../components/SectionTitle";
-import {TabMenu} from "./tabMenu/TabMenu";
+import {TabMenu, TabsStatusType} from "./tabMenu/TabMenu";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Work} from "./work/Work";
 import socialImg from "../../../assets/images/proj1.jpg";
@@ -8,33 +8,75 @@ import timerImg from "../../../assets/images/proj2.jpg";
 import {Container} from "../../../components/Container";
 import {S} from './Works_Styles'
 
-const worksItems = ["All", "LANDING PAGE", "REACT", "SPA"]
+//const tabsItems = ["All", "LANDING PAGE", "REACT", "SPA"]
+const tabsItems: Array<{status: TabsStatusType, title: string}> = [
+    {
+        title: "All",
+        status: "all"
+    },
+    {
+        title: "LANDING PAGE",
+        status: "landing"
+    },
+    {
+        title: "REACT",
+        status: "react"
+    },
+    {
+        title: "SPA",
+        status: "spa"
+    }
 
-const workData = [
+]
+
+const worksData = [
     {
         title: "Social Network",
         src: socialImg,
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        type: "spa"
     },
     {
         title: "Timer",
         src: timerImg,
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. " +
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim",
+        type: "react"
     },
 
 ]
 
 export const Works: React.FC = () => {
+    const [currentFilterStatus, setCurrentFilterStatus] = useState("all")
+    let filteredWorks = worksData
+
+    if (currentFilterStatus === "landing") {
+        filteredWorks = worksData.filter(work => work.type === "landing")
+    }
+
+    if (currentFilterStatus === "react") {
+        filteredWorks = worksData.filter(work => work.type === "react")
+    }
+
+    if (currentFilterStatus === "spa") {
+        filteredWorks = worksData.filter(work => work.type === "spa")
+    }
+
+    function changeFilterStatus (value: TabsStatusType) {
+        setCurrentFilterStatus(value)
+    }
+
     return (
         <S.Works>
             <Container>
                 <SectionTitle>My work</SectionTitle>
-                <TabMenu menuItems={worksItems}/>
+                <TabMenu tabsItems={tabsItems}
+                         changeFilterStatus={changeFilterStatus}
+                         currentFilterStatus={currentFilterStatus}/>
                 <FlexWrapper justify={"space-between"} align={"flex-start"} wrap={"wrap"}>
 
-                    {workData.map((w, index) => {
+                    {filteredWorks.map((w, index) => {
                         return <Work title={w.title}  key={index}
                                      src={w.src}
                                      text={w.text}/>
