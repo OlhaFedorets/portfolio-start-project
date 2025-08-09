2ea63681f0427d283853999f920d68d051f16531
OlhaFedorets <fedoretsolha@gmail.com>
2025-08-09 19:40:12 +0300

    change skills

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -8,40 +8,101 @@ import {Fade} from "react-awesome-reveal";
 
 const skillData = [
     {
-        iconId: "code",
+        iconId: "html",
         title: "HTML5",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        viewBox: "0 -1 32 32"
     },
     {
-        iconId: "css",
-        title: "CSS3",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-    },
-    {
-        iconId: "react",
-        title: "REACT",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        iconId: "js",
+        title: "javascript",
+        viewBox: "2 1 21 21"
     },
     {
         iconId: "typescript",
         title: "TypeScript",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        viewBox: "2 1 46 46"
+    },
+    {
+        iconId: "css",
+        title: "CSS3"
+    },
+    {
+        iconId: "react",
+        title: "REACT",
+        viewBox: "2 2 46 46"
+    },
+    {
+        iconId: "redux",
+        title: "REDUX",
+        viewBox: "0 0 32 32"
+    },
+    {
+        iconId: "redux",
+        title: "REDUX TOOLKIT",
+        viewBox: "0 0 32 32"
+    },
+    {
+        iconId: "rtk",
+        title: "RTK QUERY",
+        viewBox: "0 0 256 272"
+    },
+    {
+        iconId: "restAPI",
+        title: "REST API",
+        viewBox: "0 0 20 20"
+    },
+    {
+        iconId: "axios",
+        title: "AXIOS",
+        viewBox: "0 0 24 24"
+    },
+    {
+        iconId: "reactRouter",
+        title: "REACT ROUTER",
+        viewBox: "0 0 128 128"
+    },
+    {
+        iconId: "git",
+        title: "git",
+        viewBox: "0 0 16 16"
+    },
+     {
+        iconId: "zod",
+        title: "zod",
+        viewBox: "0 0 24 24"
+    },
+    {
+        iconId: "reactHookForm",
+        title: "react Hook Form",
+        viewBox: "0 0 24 24"
+    },
+    {
+        iconId: "materialUI",
+        title: "material UI",
+        viewBox: "0 -1 24 24"
+    },
+    {
+        iconId: "postman",
+        title: "postman",
+        viewBox: "0 0 32 32"
+    },
+    {
+        iconId: "tdd",
+        title: "unit test",
+        viewBox: "0 0 2048 2048"
+    },
+    {
+        iconId: "storyBook",
+        title: "storybook",
+        viewBox: "0 0 32 32"
     },
     {
         iconId: "styled-components",
-        title: "Styled Components",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        title: "Styled Components"
     },
     {
         iconId: "figma",
-        title: "WEB DESIGN",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, " +
-            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
+        title: "FIGMA"
     },
 
 ]
@@ -56,7 +117,7 @@ export const Skills = () => {
                         {skillData.map((s, index) => {
                             return <Skill iconId={s.iconId} key={index}
                                           title={s.title}
-                                          description={s.description}/>
+                                          viewBox={s.viewBox}/>
                         })}
                     </Fade>
                 </FlexWrapper>
